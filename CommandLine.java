49be48f5c86df365beae2cb5a6ebf8af46c06a00
Lori Oliver <loridoesitbest@gmail.com>
2019-02-18 16:53:04 -0500

    Testing master lock

---
+++ CommandLine.java
@@ -0,0 +1,9 @@
+public class CommandLine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The first argument is: " + args[0]);
+		System.out.println("The second argument is: " + args[1]);
+	}
+
+}
