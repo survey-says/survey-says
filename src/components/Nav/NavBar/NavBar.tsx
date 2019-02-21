633b143f845829cc04f83f5ea9b7c054c78e50f5
Aaron Gravelle <agrav12825@gmail.com>
2019-02-21 13:35:39 -0500

    Add link to home on navbar brand

--- src/components/Nav/NavBar/NavBar.tsx
+++ src/components/Nav/NavBar/NavBar.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import Navbar from 'react-bootstrap/Navbar';
 
 export class NavBar extends Component {
@@ -6,7 +8,9 @@ export class NavBar extends Component {
   render() {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand>Survey-Says</Navbar.Brand>
+            <LinkContainer to="/home">
+                <Navbar.Brand>Survey-Says</Navbar.Brand>
+            </LinkContainer>
         </Navbar>
     )
   }
