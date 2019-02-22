03613820ebc2899e772237cf0e033407886e699a
Aaron Gravelle <agrav12825@gmail.com>
2019-02-22 11:34:06 -0500

    fixed error of duplicate import and removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './Store';
 import './App.css';
-import LoginComponent from './components/Login/LoginContainer';
 import Layout from './containers/Layout/Layout';
-import { Route } from 'react-router';
+
 
 class App extends Component {
   render() {
