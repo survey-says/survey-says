9d1e00c8be1e9b66ef1c6db102371b15c0c948f2
Idavi2534 <idavi015@fiu.edu>
2019-02-28 20:10:16 -0500

    working on button

--- src/components/Questions/SurveyBuildComponent.tsx
+++ src/components/Questions/SurveyBuildComponent.tsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import { userInfo } from 'os';
 import $ from "jquery";
 import { string } from 'prop-types';
+import ssClient from '../../axios/ss.client';
+const ReactDOM = require('react-dom')
+
 
 export interface ISurveyProps {
     surveyTitle: string,
@@ -10,21 +13,23 @@ export interface ISurveyProps {
     handleSubmit(userInfo: {}): void
 }
 
+ 
 export class SurveyBuildComponent extends React.Component<ISurveyProps, any> {
     constructor(props) {
         super(props);
     }
   
 
-  
     
+
     handleChange = (event) => {
         let yourArray= new Array()
         var hidden = $("input:text[name=inputt]");
         $("input:checkbox[name=questionTypes]:checked").each(function(){
             yourArray.push($(this).val());
                   hidden.show()
-});
+
+  });
 
         //gets an array of the checkbox choices
        // console.log("my choices"+yourArray.join())
@@ -50,23 +55,78 @@ export class SurveyBuildComponent extends React.Component<ISurveyProps, any> {
 
        
     }
-
-
-     setVisibility=(id, box) =>{
-        let checkbox = $(id);
-        let hidden = $(box);
-        
-        if (checkbox.is(':checked')) {
-            // Show the hidden fields.
-            hidden.show();
-            // Populate the input.
-          } else {
-            // Make sure that the hidden fields are indeed
-            // hidden.
-            hidden.hide();
     
+    check=(event)=>{
+        let value = ReactDOM.findDOMNode(this.refs.selectValue).value;
+       console.log(value)
+      this.setVisibility(value)
+
+
     }
+     setVisibility=(id) =>{
+        let checked =id
+        
+        if (id==='multi') {
+            // Show the hidden fields.
+            $("#t1").show();
+            // Populate the input.
+          }
+         else {
+            // Show the hidden fields.
+            $("#t1").hide().prop('required',false)
+            // Populate the input.
+          }
+          
+          if (id==='yn') {
+            // Show the hidden fields.
+            $("#t2").show();
+            // Populate the input.
+          }else {
+            // Show the hidden fields.
+            $("#t2").hide().prop('required',false)
+            // Populate the input.
+          }
+          if (id==='agree') {
+            // Show the hidden fields.
+            $("#t3").show();
+            // Populate the input.
+          } 
+          else {
+            // Show the hidden fields.
+            $("#t3").hide().prop('required',false)
+            // Populate the input.
+          }
+           if (id==='rating') {
+            // Show the hidden fields.
+            $("#t4").show();
+            // Populate the input.
+          }
+          else {
+            // Show the hidden fields.
+            $("#t4").hide().prop('required',false)
+            // Populate the input.
+          }
+           if (id==='feedback') {
+            // Show the hidden fields.
+            $("#t5").show();
+            // Populate the input.
+          }
+          else {
+            // Show the hidden fields.
+            $("#t5").hide().prop('required',false)
+            // Populate the input.
+          }
+
+ 
 }
+
+    
+        addQuestion=(id)=>{            
+            var my_var = $(id).html();
+         console.log(my_var)
+           $(id).clone(true).appendTo("#123d");
+         
+        }
 
     handleSubmit = (event) => {
         event.preventDefault();
@@ -76,61 +136,76 @@ export class SurveyBuildComponent extends React.Component<ISurveyProps, any> {
         }
         console.log('The user survey being registered is ', userData);
         this.props.handleSubmit(userData);
+   
+
+
+   
+   
     }
 
     render() {
         return (   
         
-            <div className="container create-survey-container"> 
+            <div className="container create-survey-container" style={{width: '700px'}} > 
               <div className="jumbotron"> 
                     <h1 className="h3 mb-3 font-weight-normal">Build Your Survey</h1>
-                    <form name="form" onSubmit={this.handleSubmit} className="form-create">
-                        <div className={'form-group'}>
+                    
+                    <form  name="form" onSubmit={this.handleSubmit} className="form-create">
+                        <div id="123d"className={'form-group'}>
                         
                             <label htmlFor="surveyTitle">SurveyTitle</label>
                             <input type="surveyTitle" className="form-control" name="surveyTitle" onChange={this.handleChange} required /><br></br>
                         
                             <label htmlFor="questionTypes">Add Question Types</label><br></br>
                             
-
-                            <label className='check'><input type="checkbox"  value="1" id="multi" className="form-control" name="questionTypes" onChange={()=>(this.handleChange,this.setVisibility('#multi','#t1' ))} required />Multiple Chioce</label>
-                            <div  className='new' id="t1" style={{display:'none'}}>
-                           <input name="inputt" type="text" placeholder="Question Title (i.e. What do you like best? )" style={{width: '700px'}} required></input>
-                           <input name="inputt" type="text"  placeholder="Choices (i.e. apples, pie, chicken, ... )"  style={{width: '700px'}} required></input>
+                            
+                            <div id="allQ"style={{textAlign:'center'}}>
+                            <select ref="selectValue"  name="Question Types" id='Qtypes' onChange={this.check}>
+                            <option value="open">Please Select Your Type To Add</option>
+                           <option value="multi">Multiple Choice</option>
+                           <option value="yn">Yes or No Question</option>
+                            <option value="agree">Strongly Agree-Strongly Disagree Mini Form</option>
+                          <option value="feedback">Feedback Comment Block</option>
+                          <option value="rating">Rating</option>
+                            </select>&nbsp;&nbsp;&nbsp;&nbsp;
+                            </div>
+                           
+                             <div  className='new' id="t1" style={{display:'none'}}>
+                           <input name="inputt" type="text" placeholder="Question Title (i.e. What do you like best? )" style={{width: '500px'}} ></input>
+                           <input name="inputt" type="text"  placeholder="Choices (i.e. apples, pie, chicken, ... )"  style={{width: '500px'}} ></input>
                            </div>
 
-                            <label className='check'><input type="checkbox" value="2" id="yn"className="form-control" name="questionTypes" onChange={()=>(this.handleChange,this.setVisibility('#yn','#t2'))} required />Yes or No Question</label>                       
-                            <div className='new' id="t2" style={{display:'none'}}>
-                           <input name="inputt" type="text" placeholder="Question Title (i.e. Do you like apples?) "   style={{width: '700px'}} required></input>
+                           <div className='new' id="t2" style={{display:'none'}}>
+                           <input name="inputt" type="text" placeholder="Question Title (i.e. Do you like apples?) "   style={{width: '500px'}} ></input>
                            </div>
 
-                            <label className='check'><input type="checkbox"  value="3"id="agree"className="form-control" name="questionTypes" onChange={()=>(this.handleChange,this.setVisibility('#agree','#t3'))} required />Strongly Agree-Strongly Disagree Form</label>                      
                             <div className='new' id="t3" style={{display:'none'}}>
-                           <input name="inputt" type="text"  placeholder="Question Title (i.e. Give me your opinion )" style={{width: '700px'}}  required></input><br></br>
-                           <input name="inputt" type="text"  placeholder="Value (i.e. Average, Good, Ok, Bad )" style={{width: '700px'}}   required></input>
-                           <input name="inputt" type="text"  placeholder="Value (i.e. Average, Good, Ok, Bad )"  style={{width: '700px'}}  required></input>
-                           <input name="inputt" type="text"  placeholder="Value (i.e. Average, Good, Ok, Bad )"  style={{width: '700px'}}  required></input>
-                           <input name="inputt" type="text"  placeholder="Value (i.e. Average, Good, Ok, Bad )"  style={{width: '700px'}}  required></input> <br></br>
-                           <input name="inputt" type="text"  placeholder="Text (i.e. Product is affordable )"  style={{width: '700px'}}  required></input>
-                           <input name="inputt" type="text"  placeholder="Text (i.e. Product is does what it says )"  style={{width: '700px'}}  required></input>
-                           <input name="inputt" type="text"  placeholder="Text (i.e.Product looks like its picture )" style={{width: '700px'}}   required></input>
-                           <input name="inputt" type="text"  placeholder="Text (i.e. Product is easy to use )"  style={{width: '700px'}}  required></input><br></br>
+                           <input name="inputt" type="text"  placeholder="Question Title (i.e. Give me your opinion )" style={{width: '500px'}}   ></input><br></br>
+                           <input name="inputt" type="text"  placeholder="Value (i.e. Average, Good, Ok, Bad )" style={{width: '500px'}}    ></input>
+                           <input name="inputt" type="text"  placeholder="Value (i.e. Average, Good, Ok, Bad )"  style={{width: '500px'}}   ></input>
+                           <input name="inputt" type="text"  placeholder="Value (i.e. Average, Good, Ok, Bad )"  style={{width: '500px'}}   ></input>
+                           <input name="inputt" type="text"  placeholder="Value (i.e. Average, Good, Ok, Bad )"  style={{width: '500px'}}   ></input> <br></br>
+                           <input name="inputt" type="text"  placeholder="Text (i.e. Product is affordable )"  style={{width: '500px'}}   ></input>
+                           <input name="inputt" type="text"  placeholder="Text (i.e. Product is does what it says )"  style={{width: '500px'}}   ></input>
+                           <input name="inputt" type="text"  placeholder="Text (i.e.Product looks like its picture )" style={{width: '500px'}}    ></input>
+                           <input name="inputt" type="text"  placeholder="Text (i.e. Product is easy to use )"  style={{width: '500px'}}   ></input><br></br>
                         </div>
 
-                            <label className='check'><input type="checkbox"  value="4"id="rating"className="form-control" name="questionTypes" onChange={()=>(this.handleChange,this.setVisibility('#rating','#t4'))} required />Rating</label>                       
-                            <div className='new' id="t4" style={{display:'none'}}>
-                           <input name="inputt" type="text" placeholder="Question Title (i.e. How likely are you to recommend our product to a friend? )"  style={{width: '700px'}}  required></input>
+                             <div   className='new' id="t4" style={{display:'none'}}>
+                           <input name="inputt" type="text" placeholder="Question Title (i.e. How likely are you to recommend our product to a friend? )"  style={{width: '500px'}}   ></input>
                            </div>
 
-                            <label className='check'><input type="checkbox" value="5" id="feedback"className="form-control" name="questionTypes" onChange={()=>(this.handleChange,this.setVisibility('#feedback','#t5'))}required />Feedback Comment</label>                     
-                            <div className='new' id="t5" style={{display:'none'}}>
-                           <input name="inputt" type="text" placeholder="Question Title (i.e. Please give your feedback)"  style={{width: '700px'}}   required></input>
-                           </div>
+                          <div className='new' id="t5" style={{display:'none'}}>
+                           <input name="inputt" type="text" placeholder="Question Title (i.e. Please give your feedback)"  style={{width: '500px'}}    ></input>
+                           </div>   
+                            
+
                             </div>
-                       
+                       <div style={{textAlign:'center'}}><button name="theB" className="btn btn-primary"  onClick={()=>this.addQuestion("#allQ")}>Add Another  </button>
+                            </div>
                         
                         <div className="form-group">
-                            <button className="btn btn-primary">Create Survey</button>
+                        <br></br><br></br><button className="btn btn-primary">Create Survey</button>
 
                             <Link to="http://localhost:3000" className="btn btn-link">Cancel</Link>
                         </div>
