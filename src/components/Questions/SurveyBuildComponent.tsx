5d4b7b928648e4fa938b3a0ddb3690ac0347b849
Idavi2534 <idavi015@fiu.edu>
2019-03-01 23:51:21 -0500

    need to parse

--- src/components/Questions/SurveyBuildComponent.tsx
+++ src/components/Questions/SurveyBuildComponent.tsx
@@ -31,71 +31,60 @@ export class SurveyBuildComponent extends React.Component<ISurveyProps, any> {
     displayData = []
 
     handleChange = (event) => {
-      let script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-
-    }
-    check=(event)=>{
-      let value = ReactDOM.findDOMNode(this.refs.selectValue).value;
-     console.log(value)
-    this.setVisibility(value)
-
-    }
     
+    }
+  
  
      
 
-    setVisibility=(id) =>{
-      let checked =id
-          if (id==='multi') {
+    setVisibility=(ok,box) =>{
+      let id = $(ok).val()
+
+          if (id==1) {
             // Show the hidden fields.
+          
             $("#t1").show();
             // Populate the input.
           }
-         else {
-            // Show the hidden fields.
-            $("#t1").hide().prop('required',false)
-            // Populate the input.
-          }
+        else 
+         { $("#t1").hide(); }
           
-          if (id==='yn') {
+          if (id==2) {
             // Show the hidden fields.
             $("#t2").show();
             // Populate the input.
-          }else {
-            // Show the hidden fields.
-            $("#t2").hide().prop('required',false)
-            // Populate the input.
-          }
-          if (id==='agree') {
+           }
+          
+          if (id==3) {
             // Show the hidden fields.
             $("#t3").show();
             // Populate the input.
           } 
-          else {
-            // Show the hidden fields.
-            $("#t3").hide().prop('required',false)
-            // Populate the input.
-          }
-           if (id==='rating') {
+          // else {
+          //   // Show the hidden fields.
+          //   $("#t3").hide().prop('required',false)
+          //   // Populate the input.
+          // }
+           if (id==4) {
             // Show the hidden fields.
             $("#t4").show();
             // Populate the input.
           }
-          else {
-            // Show the hidden fields.
-            $("#t4").hide().prop('required',false)
-            // Populate the input.
-          }
-           if (id==='feedback') {
+          // else {
+          //   // Show the hidden fields.
+          //   $("#t4").hide().prop('required',false)
+          //   // Populate the input.
+          
+           if (id==5) {
             // Show the hidden fields.
             $("#t5").show();
             // Populate the input.
           }
-          else {
-            // Show the hidden fields.
-            $("#t5").hide().prop('required',false)
-            // Populate the input.
-          }
+          // else {
+          //   // Show the hidden fields.
+          //   $("#t5").hide().prop('required',false)
+          //   // Populate the input.
+          // }
 
  
 }
@@ -125,17 +114,17 @@ export class SurveyBuildComponent extends React.Component<ISurveyProps, any> {
                             <label htmlFor="questionTypes">Add Question Types</label><br></br>
 
 
-                            <label className='check'><input ref="selectValue" type="checkbox"style={{width:'20px',display: 'inline-block'}}  value="1" id="multi" className="form-control" name="questionTypes" onChange={this.check} required />Multiple Chioce</label><br></br>
+                            <label className='check'><input   type="checkbox"style={{width:'20px',display: 'inline-block'}}  value="1" id="multi" className="form-control" name="questionTypes" onChange={()=>this.setVisibility("#multi","#t1")} required />Multiple Chioce</label><br></br>
                             <div  className='new' id="t1" style={{display:'none'}}>
-                           <input name="inputt" type="text" placeholder="Question Title (i.e. What do you like best? )" style={{width: '700px'}} required></input>
-                           <input name="inputt" type="text"  placeholder="Choices (i.e. apples, pie, chicken, ... )"  style={{width: '700px'}} required></input>
+                           <input name="inputt" type="text" placeholder="Question Title (i.e. What do you like best? )" style={{width: '500px'}} required></input>
+                           <input name="inputt" type="text"  placeholder="Choices (i.e. apples, pie, chicken, ... )"  style={{width: '500px'}} required></input>
                       </div>
-                      <label className='check'><input ref="selectValue"style={{width:'20px',display: 'inline-block'}} type="checkbox" value="2" id="yn"className="form-control" name="questionTypes"onChange={this.check} required />Yes or No Question</label>     <br></br>                  
+                      <label className='check'><input  style={{width:'20px',display: 'inline-block'}} type="checkbox" value="2" id="yn"className="form-control" name="questionTypes"onChange={()=>this.setVisibility("#yn","#t2")} required />Yes or No Question</label>     <br></br>                  
                             <div className='new' id="t2" style={{display:'none'}}>
-                           <input name="inputt" type="text" placeholder="Question Title (i.e. Do you like apples?) "   style={{width: '700px'}} required></input>
+                           <input name="inputt" type="text" placeholder="Question Title (i.e. Do you like apples?) "   style={{width: '500px'}} required></input>
 
-</div>
-<label className='check'><input type="checkbox"ref="selectValue" style={{width:'20px',display: 'inline-block'}} value="3"id="agree"className="form-control" name="questionTypes"onChange={this.check} required />Strongly Agree-Strongly Disagree Form</label>   <br></br>                   
+                          </div>
+                          <label className='check'><input type="checkbox"  style={{width:'20px',display: 'inline-block'}} value="3"id="agree"className="form-control" name="questionTypes"onChange={()=>this.setVisibility("#agree","#t3")} required />Strongly Agree-Strongly Disagree Form</label>   <br></br>                   
                           
                            <div className='new' id="t3" style={{display:'none'}}>
                            <input name="inputt" type="text"  placeholder="Question Title (i.e. Give me your opinion )" style={{width: '500px'}}   ></input><br></br>
@@ -148,12 +137,12 @@ export class SurveyBuildComponent extends React.Component<ISurveyProps, any> {
                            <input name="inputt" type="text"  placeholder="Text (i.e.Product looks like its picture )" style={{width: '500px'}}    ></input>
                            <input name="inputt" type="text"  placeholder="Text (i.e. Product is easy to use )"  style={{width: '500px'}}   ></input><br></br>
                         </div>
-                        <label className='check'><input ref="selectValue" style={{width:'20px',display: 'inline-block'}} type="checkbox"  value="4"id="rating"className="form-control" name="questionTypes"onChange={this.check}required />Rating</label>      <br></br>                 
+                        <label className='check'><input   style={{width:'20px',display: 'inline-block'}} type="checkbox"  value="4"id="rating"className="form-control" name="questionTypes"onChange={()=>this.setVisibility("#rating","#t4")}required />Rating</label>      <br></br>                 
 
                         <div   className='new' id="t4" style={{display:'none'}}>
                            <input name="inputt" type="text" placeholder="Question Title (i.e. How likely are you to recommend our product to a friend? )"  style={{width: '500px'}}   ></input>
                            </div>
-                           <label className='check'><input ref="selectValue" type="checkbox" style={{width:'20px',display: 'inline-block'}}value="5"id="feedback"className="form-control" name="questionTypes"onChange={this.check}required />Feedback</label>  <br></br>                     
+                           <label className='check'><input   type="checkbox" style={{width:'20px',display: 'inline-block'}}value="5"id="feedback"className="form-control" name="questionTypes"onChange={()=>this.setVisibility("#feedback","#t5")}required />Feedback</label>  <br></br>                     
 
                        <div className='new' id="t5" style={{display:'none'}}>
                            <input name="inputt" type="text" placeholder="Question Title (i.e. Please give your feedback)"  style={{width: '500px'}}    ></input>
